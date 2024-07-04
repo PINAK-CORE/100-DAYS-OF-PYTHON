8062337a344345c4d0f976866fdef6ce7b903627
PINAK TILAVAT <dingo.in4k@gmail.com>
2024-07-04 22:52:50 +0530

    Day 4 ended

--- DAY 004/main.py
+++ DAY 004/main.py
@@ -1,6 +1,6 @@
 # DAY 4 : ROCK PAPER SCISSORS
 
-import random as r
+import random 
 
 rock = '''
     _______
@@ -29,9 +29,145 @@ scissors = '''
 ---.__(___)
 '''
 
-game_mode = input("Are you Ready To Play Rock Paper Scissors ? [1] lets roll [2] nahh")
+game_mode = input("Are you Ready To Play Rock Paper Scissors ? [1] lets roll [2] nahh : ")
 
 
 while(game_mode == '1'):
     
-    com_choice = r.ran
+    p1_choice = random.randint(1,3)
+    print("Enter 0 to exit this Game")
+    p2_choice = int(input
+                    (
+                        '''
+                        Enter your choice 
+                        [1] Rock
+                        [2] Paper
+                        [3] Scissors
+\n
+> Your Choice : '''
+                    ))
+    
+    if(p2_choice == 0):
+        break
+
+    if(p1_choice == p2_choice ):
+
+        if(p1_choice == 1):
+            print(
+                f'''
+                {rock} Computer
+
+                v/s
+
+                {rock} YOU
+                '''
+            )
+        if(p1_choice == 2):
+            print(
+                f'''
+                {paper} Computer
+
+                v/s
+                
+                {paper} YOU
+                '''
+            )
+        if(p1_choice == 3):
+            print(
+                f'''
+                {scissors} Computer
+
+                v/s
+                
+                {scissors} YOU
+                '''
+            )
+
+        print("Result : tie")
+
+    elif(p1_choice == 1 and p2_choice == 2):
+
+        print(
+                f'''
+                {rock} Computer
+
+                v/s
+                
+                {paper} YOU
+                '''
+            )
+
+        print("Result : You Win !!!")
+
+    elif(p1_choice == 1 and p2_choice == 3):
+
+        print(
+                f'''
+                {rock} Computer
+
+                v/s
+                
+                {scissors} YOU
+                '''
+            )
+
+        print("Result : Compter Wins !!")
+
+    elif(p1_choice == 2 and p2_choice == 1):
+
+        print(
+                f'''
+                {paper} Computer
+
+                v/s
+                
+                {rock} YOU
+                '''
+            )
+
+        print("Result : Computer Win !!!")
+
+    elif(p1_choice == 2 and p2_choice == 3):
+
+        print(
+                f'''
+                {paper} Computer
+
+                v/s
+                
+                {scissors} YOU
+                '''
+            )
+        
+        print("Result : You Wins !!!")
+    
+    elif(p1_choice == 3 and p2_choice == 1):
+
+        print(
+                f'''
+                {scissors} Computer
+
+                v/s
+                
+                {rock} YOU
+                '''
+            )
+        
+        print("Result : You Wins !!")
+
+    elif(p1_choice == 3 and p2_choice == 2):
+
+        print(
+                f'''
+                {scissors} Computer
+
+                v/s
+                
+                {paper} YOU
+                '''
+            )
+
+        print("Result : Computer Wins !!")
+
+print("\n THANK YOU FOR PLAYTING THIS GAME ")
+    
