dad17ed34fe408ada6081748e114d3f4449756b4
PINAK TILAVAT <dingo.in4k@gmail.com>
2024-07-07 23:00:41 +0530

    day 6 (WIP)

--- DAY 007/main.py
+++ DAY 007/main.py
@@ -1,1 +1,14 @@
 #DAY 7 HANGMEN
+import random
+temp = ["apple" , "banana" , "mango" , "kiwi"]
+
+word = random.choice(temp)
+
+
+guess = []
+for i in range(len(word)):
+    guess.append("_")
+
+
+print(word)
+print(guess)
