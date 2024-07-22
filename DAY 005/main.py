394bd870e7e4ebd4301d3ec2dd73d7d252874074
PINAK TILAVAT <dingo.in4k@gmail.com>
2024-07-22 07:26:46 +0530

    Revert "DAY 5 (WIP)"
    This reverts commit c1b119b4084278d0ca22fac99ab1cf7dc6bbbea7.

--- DAY 005/main.py
+++ DAY 005/main.py
@@ -4,4 +4,3 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-no_letters = int("How many Letters you want in Your ")
